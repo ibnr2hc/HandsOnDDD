968063678130cabffdaf5d4419e4f30df4251278
Takumi Suzuki <43519598+ibnr2hc@users.noreply.github.com>
2023-07-13 19:54:45 +0900

    aa

--- init.sql
+++ init.sql
@@ -3,19 +3,19 @@ ALTER DATABASE library CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE library;
 
 CREATE TABLE users (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50),
     email VARCHAR(50)
 ) CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 CREATE TABLE books (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(100),
     status ENUM('available', 'borrowed')
 ) CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 CREATE TABLE loan_history (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT,
     book_id INT,
     borrow_date DATE,
@@ -26,13 +26,17 @@ CREATE TABLE loan_history (
 ) CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- テストデータの挿入
-INSERT INTO users (id, name, email) VALUES 
-(1, 'Daniel Craig', 'daniel@example.com'),
-(2, 'Mads Mikkelsen', 'mads@example.com');
+INSERT INTO users (name, email) VALUES 
+('Daniel Craig', 'daniel@example.com'),
+('Mads Mikkelsen', 'mads@example.com');
 
-INSERT INTO books (id, title, status) VALUES 
-(1, 'Attack on Titan', 'available'),
-(2, 'JoJo', 'borrowed');
+INSERT INTO books (title, status) VALUES 
+('Attack on Titan', 'available'),
+('JoJo', 'available'),
+('Dragon Ball', 'available'),
+('TEST TITLE A', 'available'),
+('TEST TITLE B', 'available'),
+('TEST TITLE C', 'available');
 
-INSERT INTO loan_history (id, user_id, book_id, borrow_date, due_date, return_date) VALUES 
-(1, 1, 2, '2023-06-01', '2023-06-08', NULL);
+INSERT INTO loan_history (user_id, book_id, borrow_date, due_date, return_date) VALUES 
+(1, 2, '2023-06-01', '2023-06-08', NULL);
