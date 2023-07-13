4edc00ea3c87f4ff15c46fd5d8d8f28d870c9345
Takumi Suzuki <43519598+ibnr2hc@users.noreply.github.com>
2023-07-13 19:45:41 +0900

    ue

--- src/application/usecase/book_list.py
+++ src/application/usecase/book_list.py
@@ -1,18 +1,19 @@
 from domain.book_aggregate.book import Book
 from application.usecase.book_list_dto import BookListDTO
 
+from typing import List
+
 
 class BookListUseCase:
     def __init__(self, library_repository):
         self.library_repository = library_repository
 
-    def execute(self) -> BookListDTO:
+    def execute(self) -> List[Book]:
         """ 本の一覧を返す
 
         Returns:
-            (List[BookDTO]): 本のDTOのリスト
+            (List[Book]): 本Entityのリスト
         """
         # 本の一覧を取得する
         library = self.library_repository.get_library()
-        # 本の一覧をBookDTOのリストに変換する
         return library.list_books()
